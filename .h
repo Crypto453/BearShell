61ec11a0f3f38892a984e34ce74384468e8a8576
DD123M2 <88851864+Crypto453@users.noreply.github.com>
2021-10-20 22:46:57 +0000

    Add files via upload

---
+++ .h
@@ -0,0 +1,148 @@
+commands
+
+--generate reverse shell tcp linux
+|
+- generates a malicious payload, with the function of giving you a
+- reverse shell through the tcp IP protocol, only for linux machines
+
+--start listening port
+|
+-start a listening port at 127.0.0.1
+
+--pwd
+|
+-view current directory
+
+--cd
+|
+-change directory
+
+--dir
+|
+-list
+
+--network processes
+|
+-view all current network processes
+
+--principal processes
+|
+-see main processes of the computer
+
+--full processes
+|
+-see absolutely all program processes
+
+--firefox
+|
+-Launch firefox browser
+
+--clear
+|
+-Clean screen
+
+--banner
+|
+-works to clean screen and change banner color
+
+--camera
+|
+-open camera
+
+--mydisks
+|
+-see discs present in the machine
+
+--connect by ssh
+|
+-connect to ssh server
+
+--connect ssh grafical
+|
+-connect to ssh server, with the function of running graphic applications
+
+--install
+|
+-install programs from apt package manager
+
+--up disk
+|
+-mount disk or usb drive
+
+--down disk
+|
+-umount disk or usb drive
+
+--boot usb
+|
+-works to create a bootable usb of some operating system
+
+--view dpkg
+|
+-works to see the list of downloaded packages
+
+--ssh config
+|
+-allows you to start, restart and stop the ssh server
+
+--port scan
+|
+-works to scan the ports of an IP address
+
+--pn
+|
+-makes a simple ping to an address or site
+
+--close
+|
+- close shellBear
+
+--kill
+|
+-kill process by number
+
+--pkill
+|
+-kill process by name
+
+--editor
+|
+-open the text editor subl text
+
+--network up
+|
+-works to turn on the network card
+
+--network down
+|
+-works to turn off the network card
+
+--git
+|
+-is to clone a github repository
+
+--download
+|
+-its function is to download files from the internet
+
+--my ip address
+|
+-it works so you can see your public IP address
+
+--update
+|
+-update computer packages
+
+--run program
+|
+-run a program in any language
+
+--sendd file by ssh
+|
+-send a file by ssh
+
+--BearShell
+|
+-info by BearShell
+
+
